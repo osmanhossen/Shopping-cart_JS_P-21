7f2f493acb17eceea512e0436494e8498c5fd8dc
osmanhossen <>
2023-06-10 15:03:17 +0600

    shopping_cart added

--- js/total.js
+++ js/total.js
@@ -1,1 +1,20 @@
-console.log('total page added')
+/* Jodi Check_out Button e click Total Check korte cay ai function uncomment korte hobe ---and all Plus_Minus Button ti teke <calculateSubTotal()> aita dlete or comment kore dite hobe*/
+//--------------------------------------------------------
+document.getElementById("btn_Check_Out").addEventListener("click", function () {
+  console.log("please uncomment the Coding inside for active this button ");
+  // calculate Total
+  // const currentPhoneTotal = getTextElementValueById("mobile_total_price");
+  // const currentCaseTotal = getTextElementValueById("total_price");
+
+  // const currentSubTotal = currentCaseTotal + currentPhoneTotal;
+  // setTextElementValueById("sub_Total_amount", currentSubTotal);
+
+  // // calculate Tax
+  // const taxAmountString = (currentSubTotal * 0.05).toFixed(2);
+  // const taxAmount = parseFloat(taxAmountString);
+  // setTextElementValueById("tax_amount", taxAmount);
+
+  // // calculate pay total
+  // const finalAmount = currentSubTotal + taxAmount;
+  // setTextElementValueById("total_pay_amount", finalAmount);
+});
